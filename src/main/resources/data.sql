277759599746614a5c713a7806f1333b225d1b84
Josh Long <josh@joshlong.com>
2020-01-08 19:26:37 -0800

    data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+insert into person(id, name)
+values (1, 'Huiren');
+insert into person(id, name)
+values (2, 'Josh');
+
+insert into photo (id, file_name)
+values (1, 'test.jpg');
+insert into photo (id, file_name)
+values (2, 'a-profile-photo.jpg');
+insert into photo (id, file_name)
+values (3, 'a-name.jpg');
+insert into photo (id, file_name)
+values (4, 'profile.jpg');
+insert into photo (id, file_name)
+values (5, 'why.jpg');
+insert into photo (id, file_name)
+values (6, '0e4u2022-profile.jpg');
+
+
+
+insert into person_photos (person_id, photos_id)
+values (1, 1);
+insert into person_photos (person_id, photos_id)
+values (1, 2);
+insert into person_photos (person_id, photos_id)
+values (1, 3);
+insert into person_photos (person_id, photos_id)
+values (2, 4);
+insert into person_photos (person_id, photos_id)
+values (2, 5);
